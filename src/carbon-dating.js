e9b1d6b3335acc4b356b8fca3c5562fb242e6d62
Vitaly Yakubovski <falkon777.13@gmail.com>
2021-09-14 13:23:20 +0300

    feat: add new code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,18 +6,23 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (typeof(sampleActivity) !== 'string' || +sampleActivity > 15 || +sampleActivity <= 0 || isNaN(+sampleActivity)) {
+    return false;
+  } else {
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD));
+    return +t;
+  }
 }
--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  var count = 0;
+    s1 = s1.split('');
+    s2 = s2.split('');
+
+    s1.forEach(e => {
+      if (s2.includes(e)) {
+        count++;
+        s2.splice(s2.indexOf(e), 1);
+      }
+    });
+
+    return count;
 }
